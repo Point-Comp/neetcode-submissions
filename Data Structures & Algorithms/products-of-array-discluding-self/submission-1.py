bcfa82b3f5f3addda7dda32739745088f7a2e89d
Point-Comp <147598796+Point-Comp@users.noreply.github.com>
2026-09-16 11:31:29 +0100

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        pre=[]
+        pro=[]
+        prod=1
+        solar=[]
+
+        pre.append(1)
+        for i in range(len(nums)):
+            prod*=nums[i]
+            pre.append(prod)
+        prod=1
+        
+        for j in reversed(range(len(nums))):
+            prod*=nums[j]
+            pro.append(prod)
+        pro.reverse()
+        pro.append(1)
+
+        for k in range(len(pre)):
+            if k+1<len(pro):
+                solar.append(pre[k]*pro[k+1])
+
+
+        return solar
+
+
+
+        
+
